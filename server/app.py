4e4eaa06cd79e72ac74805b87449811a6331b6c2
calebmutisya <mutisyacalebmusau@gmail.com>
2024-01-11 14:15:44 +0300

    Post succesful

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from models import db, User, Review, Game
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -52,20 +52,41 @@ def game_by_id(id):
 
     return response
 
-@app.route('/reviews')
+@app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
 
-    reviews = []
-    for review in Review.query.all():
-        review_dict = review.to_dict()
-        reviews.append(review_dict)
+    if request.method == 'GET':
+        reviews = []
+        for review in Review.query.all():
+            review_dict = review.to_dict()
+            reviews.append(review_dict)
 
-    response = make_response(
-        reviews,
-        200
-    )
+        response = make_response(
+            reviews,
+            200
+        )
 
-    return response
+        return response
+
+    elif request.method == 'POST':
+        new_review = Review(
+            score=request.json.get("score"),
+            comment=request.json.get("comment"),
+            game_id=request.json.get("game_id"),
+            user_id=request.json.get("user_id"),
+        )
+
+        db.session.add(new_review)
+        db.session.commit()
+
+        review_dict = new_review.to_dict()
+
+        response = make_response(
+            review_dict,
+            201
+        )
+
+        return response
 
 @app.route('/users')
 def users():
@@ -81,6 +102,42 @@ def users():
     )
 
     return response
+
+
+@app.route('/reviews/<int:id>', methods=['GET', 'DELETE'])
+def review_by_id(id):
+    review= Review.query.filter(Review.id == id).first()
+    if request.method == 'GET':
+        review_dict = review.to_dict()
+
+        response = make_response(
+            review_dict,
+            200
+        )
+
+        return response
+
+    elif request.method == 'DELETE':
+        db.session.delete(review)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Review deleted."
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+
+        return response
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
